4eb02f4d44b2ac1138f25dc71da805228948f346
yoavbenNun <yoav.bennun535@gmail.com>
2026-05-30 16:43:55 +0300

    TES-53: QML Infrastructure & Backtracking Engine Integration

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QDebug>
+#include <QMetaType>
+#include <QQmlContext> 
+#include "scheduler/ScheduleGenerator.h"
+
+Q_DECLARE_METATYPE(std::vector<std::vector<int>>)
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    // טעינה ישירה מהנתיב שבו הקובץ נמצא
+    SchedulingBlock emptyBlock; 
+    ScheduleGenerator myGenerator(emptyBlock);
+
+    qRegisterMetaType<std::vector<std::vector<int>>>("std::vector<std::vector<int>>");
+
+    engine.rootContext()->setContextProperty("scheduler", &myGenerator);
+
     qDebug() << "Loading Main.qml from disk...";
     engine.load(QStringLiteral("Main.qml"));
 
