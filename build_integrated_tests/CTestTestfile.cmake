33639c0cf3215277405350a98b5a39288710e1dc
lidor9551 <lidor9551@gmail.com>
2026-06-15 00:04:26 +0300

    Ignore local build artifacts

--- build_integrated_tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/user/Desktop/Test_Scheduling_Proj
-# Build directory: C:/Users/user/Desktop/Test_Scheduling_Proj/build_integrated_tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
